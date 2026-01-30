a611ddf3521812dfda9a3059556d7095e8f9f861
taypro12 <buingoctay7@gmail.com>
2026-01-30 22:05:11 +0700

    fix vite base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,20 @@
 import path from 'path'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig({
+  base: '/', // 👈 BẮT BUỘC
 
-  return {
-    base: '/',   // 👈 CHỈ ĐỂ "/"
+  plugins: [react()],
 
-    plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+  },
 
-    server: {
-      port: 3000,
-      host: true,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
-    },
-  }
+  },
 })
