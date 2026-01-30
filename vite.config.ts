1588da6afe125a47cfde8dbaaf0112bf11b81349
taypro12 <buingoctay7@gmail.com>
2026-01-30 22:12:45 +0700

    fix vite structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // 👈 BẮT BUỘC
-
   plugins: [react()],
-
-  server: {
-    port: 3000,
-    host: true,
-  },
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+  build: {
+    outDir: 'dist'
+  }
 })
